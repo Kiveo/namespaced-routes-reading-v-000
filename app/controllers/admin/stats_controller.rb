77b31bf84707fc9db59c7df35d638cbd1daa2f1b
Maverick Tesini <mavericktesini@gmail.com>
2018-08-03 18:27:49 +0000

    automatically backed up by learn

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,5 +1,8 @@
-class Admin::StatsController < ApplicationController
-  def index
-  end
+class StatsController < ApplicationController
 
+  def index
+    @post_count = Post.count
+    @authors_count = Author.count
+    @last_post = Post.last
+  end
 end
